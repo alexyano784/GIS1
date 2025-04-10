e39e039b2112820456ca37cdda9a4c79e1b5ed65
alexyano784 <alexyano@sas.upenn.edu>
2025-04-10 15:27:24 -0400

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+  center: [35.911034, -79.062080],
+  zoom: 15,
+});
+
+var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
